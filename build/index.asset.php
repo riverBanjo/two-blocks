ef087c7d021085aa23aa09aa4757446cad755cf4
River Fisher <river.bass.fisher@gmail.com>
2021-08-24 00:45:00 -0400

    Starting over but this time actually learning

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ac87e6f7b5c7c1731c0c7bd5b4ad3c86');
